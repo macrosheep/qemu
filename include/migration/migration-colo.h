d709f3cfad3bcfb3e76b32a26db461735c5631f2
Yang Hongyang <yanghy@cn.fujitsu.com>
2014-07-23 18:33:35 +0800

    COLO save: reuse migration bitmap under colo checkpoint
    reuse migration bitmap under colo checkpoint, only send dirty pages
    per-checkpoint.
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

--- include/migration/migration-colo.h
+++ include/migration/migration-colo.h
@@ -21,10 +21,12 @@ bool colo_supported(void);
 /* save */
 bool migrate_use_colo(void);
 void colo_init_checkpointer(MigrationState *s);
+bool colo_is_master(void);
 
 /* restore */
 bool restore_use_colo(void);
 void restore_exit_colo(void);
+bool colo_is_slave(void);
 
 void colo_process_incoming_checkpoints(QEMUFile *f);
 

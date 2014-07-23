de3706ed3782e90ce5be4bf0762b25581feec2bf
Yang Hongyang <yanghy@cn.fujitsu.com>
2014-07-23 21:08:04 +0800

    COLO ram cache: implement colo ram cache on slave
    The ram cache was initially the same as PVM's memory. At
    checkpoint, we cache the dirty memory of PVM into ram cache
    (so that ram cache always the same as PVM's memory at every
    checkpoint), flush cached memory to SVM after we received
    all PVM dirty memory (only needed to flush memory that was
    both dirty on PVM and SVM since last checkpoint).
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

--- migration/colo.c
+++ migration/colo.c
@@ -475,6 +475,8 @@ void *colo_process_incoming_checkpoints(void *opaque)
         goto out;
     }
 
+    create_and_init_ram_cache();
+
     ret = colo_ctl_put(ctl, COLO_READY);
     if (ret) {
         goto out;
@@ -565,6 +567,8 @@ out:
     if (fb) {
         qemu_fclose(fb);
     }
+
+    release_ram_cache();
 
     if (ctl) {
         qemu_fclose(ctl);

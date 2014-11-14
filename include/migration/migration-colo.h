5caa40e9697c8f90ad885be1370d111aa90eb9c9
Yang Hongyang <yanghy@cn.fujitsu.com>
2014-11-14 14:41:54 +0800

    COLO failover: add failover/heartbeat module
    add failover/heartbeat module
    master:
    checkpoint channel: live/dead
    heartbeat channel : dead
    master take over
    
    checkpoint channel: dead
    heartbeat channel : live/dead
    master take over
    
    slave:
    checkpoint channel: dead
    heartbeat channel : live
    slave exit
    
    checkpoint channel: live/dead
    heartbeat channel : dead
    slave failover
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

--- include/migration/migration-colo.h
+++ include/migration/migration-colo.h
@@ -13,6 +13,9 @@
 
 #include "qemu-common.h"
 #include "migration/migration.h"
+#include "block/coroutine.h"
+#include "qemu/thread.h"
+#include "qemu/main-loop.h"
 
 void colo_info_mig_init(void);
 
@@ -38,5 +41,8 @@ bool colo_is_slave(void);
 /* ram cache */
 void create_and_init_ram_cache(void);
 void release_ram_cache(void);
+
+/* failover */
+void colo_do_failover(MigrationState *s);
 
 #endif

a3bfc2ff5c28b08c693f2606f432cd403a1436e0
Yang Hongyang <yanghy@cn.fujitsu.com>
2014-11-14 14:41:54 +0800

    COLO failover: add failover/heartbeat module
    add failover/heartbeat module
    master:
    checkpoint channel: live/dead
    heartbeat channel : dead
    master take over
    
    checkpoint channel: dead
    heartbeat channel : live/dead
    master take over
    
    slave:
    checkpoint channel: dead
    heartbeat channel : live
    slave exit
    
    checkpoint channel: live/dead
    heartbeat channel : dead
    slave failover
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

---
+++ include/migration/migration-failover.h
@@ -0,0 +1,20 @@
+/*
+ *  COarse-grain LOck-stepping Virtual Machines for Non-stop Service (COLO)
+ *  (a.k.a. Fault Tolerance or Continuous Replication)
+ *
+ * Copyright (C) 2015 FUJITSU LIMITED
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later.  See the COPYING file in the top-level directory.
+ */
+
+#ifndef MIGRATION_FAILOVER_H
+#define MIGRATION_FAILOVER_H
+
+#include "qemu-common.h"
+
+void failover_request_set(void);
+void failover_request_clear(void);
+bool failover_request_is_set(void);
+
+#endif

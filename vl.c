d6e4274e834ee2ddb9b044fe5a50bc9c2f9c899d
Yang Hongyang <yanghy@cn.fujitsu.com>
2015-07-29 11:05:48 +0800

    init/cleanup of netfilter object
    Add a netfilter object based on QOM.
    
    A netfilter is attached to a netdev, captures all network packets
    that pass through the netdev. When we delete the netdev, we also
    delete the netfilter object attached to it, because if the netdev is
    removed, the filter which attached to it is useless.
    
    QTAILQ_ENTRY global_list but used by filter layer, so that we can
    manage all filters together.
    QTAILQ_ENTRY next used by netdev, filter belongs to the specific netdev is
    in this queue.
    
    Also init delayed object after net_init_clients, because netfilters need
    to be initialized after net clients initialized.
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>
    ---
    v10: use QOM for netfilter
    v9: use flat union instead of simple union in QAPI schema
    v8: include vhost_net header
    v7: add check for vhost
        fix error propagate bug
    v6: add multiqueue support (net_filter_init1)
    v5: remove model from NetFilterState
        add a sent_cb param to receive_iov API

--- vl.c
+++ vl.c
@@ -2783,6 +2783,7 @@ static bool object_create_initial(const char *type)
     if (g_str_equal(type, "rng-egd")) {
         return false;
     }
+    /* TODO: reture false for concrete netfilters */
     return true;
 }
 
@@ -4302,12 +4303,6 @@ int main(int argc, char **argv, char **envp)
         exit(0);
     }
 
-    if (qemu_opts_foreach(qemu_find_opts("object"),
-                          object_create,
-                          object_create_delayed, NULL)) {
-        exit(1);
-    }
-
     machine_opts = qemu_get_machine_opts();
     if (qemu_opt_foreach(machine_opts, machine_set_property, current_machine,
                          NULL)) {
@@ -4410,6 +4405,12 @@ int main(int argc, char **argv, char **envp)
     atexit(&net_cleanup);
 
     if (net_init_clients() < 0) {
+        exit(1);
+    }
+
+    if (qemu_opts_foreach(qemu_find_opts("object"),
+                          object_create,
+                          object_create_delayed, NULL)) {
         exit(1);
     }
 

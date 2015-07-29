96be79536ba0edf593b17a3fa15a9b380ddfab9a
Yang Hongyang <yanghy@cn.fujitsu.com>
2015-07-29 14:47:11 +0800

    netfilter: add a netbuffer filter
    This filter is to buffer/release packets, this feature can be used
    when using MicroCheckpointing, or other Remus like VM FT solutions, you
    can also use it to simulate the network delay.
    It has an interval option, if supplied, this filter will release
    packets by interval.
    
    Usage:
     -netdev tap,id=bn0
     -netfilter buffer,id=f0,netdev=bn0,interval=1000
    
    NOTE:
     the scale of interval is microsecond.
     API to release packets and interval support will be added
     by the following 2 patches.
    
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>

---
+++ net/filter-buffer.c
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2015 FUJITSU LIMITED
+ * Author: Yang Hongyang <yanghy@cn.fujitsu.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later.  See the COPYING file in the top-level directory.
+ */
+
+#include "net/filter.h"
+#include "net/queue.h"
+#include "filters.h"
+#include "qemu-common.h"
+#include "qemu/error-report.h"
+
+typedef struct FILTERBUFFERState {
+    NetFilterState nf;
+    NetClientState dummy; /* used to send buffered packets */
+    NetQueue *incoming_queue;
+    NetQueue *inflight_queue;
+} FILTERBUFFERState;
+
+static void packet_send_completed(NetClientState *nc, ssize_t len)
+{
+    return;
+}
+
+static void filter_buffer_flush(NetFilterState *nf)
+{
+    FILTERBUFFERState *s = DO_UPCAST(FILTERBUFFERState, nf, nf);
+    NetQueue *queue = s->inflight_queue;
+    NetPacket *packet;
+
+    while (queue && !QTAILQ_EMPTY(&queue->packets)) {
+        packet = QTAILQ_FIRST(&queue->packets);
+        QTAILQ_REMOVE(&queue->packets, packet, entry);
+        queue->nq_count--;
+
+        if (packet->flags & QEMU_NET_PACKET_FLAG_RAW) {
+            qemu_send_packet_raw(&s->dummy, packet->data, packet->size);
+        } else {
+            qemu_send_packet_async(&s->dummy, packet->data, packet->size,
+                                   packet->sent_cb);
+        }
+
+        /*
+         * now that we pass the packet to sender->peer->incoming_queue, we
+         * don't care the reture value here, because the peer's queue will
+         * take care of this packet
+         */
+        g_free(packet);
+    }
+
+    if (queue && QTAILQ_EMPTY(&queue->packets)) {
+        g_free(queue);
+        s->inflight_queue = NULL;
+    }
+}
+
+/* filter APIs */
+static ssize_t filter_buffer_receive(NetFilterState *nf,
+                                     NetClientState *sender,
+                                     unsigned flags,
+                                     const uint8_t *data,
+                                     size_t size)
+{
+    FILTERBUFFERState *s = DO_UPCAST(FILTERBUFFERState, nf, nf);
+    NetQueue *queue = s->incoming_queue;
+
+    if (!sender->info) {
+        /* This must be a dummy NetClientState, do nothing */
+        return 0;
+    }
+
+    if (sender->info->type == NET_CLIENT_OPTIONS_KIND_NIC) {
+        /* we only buffer guest output packets */
+        qemu_net_queue_append(queue, sender, flags, data, size,
+                              packet_send_completed);
+        /* Now that we have buffered the packet, return sucess */
+        return size;
+    }
+
+    return 0;
+}
+
+static void filter_buffer_cleanup(NetFilterState *nf)
+{
+    FILTERBUFFERState *s = DO_UPCAST(FILTERBUFFERState, nf, nf);
+
+    /* flush inflight packets */
+    filter_buffer_flush(nf);
+    /* flush incoming packets */
+    s->inflight_queue = s->incoming_queue;
+    s->incoming_queue = NULL;
+    filter_buffer_flush(nf);
+
+    return;
+}
+
+
+static NetFilterInfo net_filter_buffer_info = {
+    .type = NET_FILTER_OPTIONS_KIND_BUFFER,
+    .size = sizeof(FILTERBUFFERState),
+    .receive = filter_buffer_receive,
+    .cleanup = filter_buffer_cleanup,
+};
+
+int net_init_filter_buffer(const NetFilterOptions *opts, const char *name,
+                           NetClientState *netdev, Error **errp)
+{
+    NetFilterState *nf;
+    FILTERBUFFERState *s;
+
+    assert(opts->kind == NET_FILTER_OPTIONS_KIND_BUFFER);
+
+    nf = qemu_new_net_filter(&net_filter_buffer_info, netdev, "buffer", name);
+    s = DO_UPCAST(FILTERBUFFERState, nf, nf);
+    /*
+     * we need the dummy NetClientState to send packets in order to avoid
+     * receive packets again.
+     * we are buffering guest output packets, our buffered packets should be
+     * sent to real network backend, so dummy's peer should be that backend.
+     */
+    s->dummy.peer = netdev;
+    s->incoming_queue = qemu_new_net_queue(nf);
+
+    return 0;
+}
---
+++ net/filters.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015 FUJITSU LIMITED
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later.  See the COPYING file in the top-level directory.
+ */
+
+#ifndef QEMU_NET_FILTERS_H
+#define QEMU_NET_FILTERS_H
+
+#include "net/net.h"
+#include "net/filter.h"
+
+int net_init_filter_buffer(const NetFilterOptions *opts, const char *name,
+                           NetClientState *netdev, Error **errp);
+
+#endif /* QEMU_NET_FILTERS_H */
